0c951b07eb64e119553844a32a74bb1ad0821d1a
Adrian <adrian>
2026-05-05 21:15:17 +0200

    Add front and back as submodules

--- Pokestore-back/src/routes/Commentroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const CommentController = require('../controllers/CommentController');
-
-// Ruta para agregar un comentario
-router.post('/add-comment', CommentController.addComment);
-
-module.exports = router;
